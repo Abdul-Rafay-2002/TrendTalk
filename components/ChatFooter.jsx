d660c8d5b764f1e27b710bd518c9caf666d52404
Abdul Rafay <rafaymalick68@gmail.com>
2023-09-03 11:05:26 -0600

    new changes

--- components/ChatFooter.jsx
+++ components/ChatFooter.jsx
@@ -2,25 +2,71 @@ import React, { useState } from 'react';
 import Icons from './Icons';
 import { CgAttachment } from 'react-icons/cg';
 import { HiOutlineEmojiHappy } from 'react-icons/hi';
-import Composebar from './Composebar';
 import EmojiPicker from 'emoji-picker-react';
 import ClickAwayListener from 'react-click-away-listener';
+import Composebar from './Composebar';
+import { IoClose } from 'react-icons/io5';
+import { useChatContext } from '@/context/chatContext';
 
 const ChatFooter = () => {
 	const handleClickAway = () => {
 		setUseEmojiPicker(false);
 	};
 	const [useEmojiPicker, setUseEmojiPicker] = useState(false);
-	const onEmojiClick = () => {};
+	const {
+		isTyping,
+		editMsg,
+		setEditMsg,
+		inputText,
+		setInputText,
+		setAttachment,
+		attachmentPreview,
+		setAttachmentPreview,
+	} = useChatContext();
+	const onEmojiClick = (emojiData) => {
+		let text = inputText;
+		setInputText((text += emojiData.emoji));
+	};
+	const onFileChange = (e) => {
+		const file = e.target.files[0];
+		setAttachment(file);
 
+		if (file) {
+			const blobUrl = URL.createObjectURL(file);
+			setAttachmentPreview(blobUrl);
+		}
+	};
 	return (
-		<div className=' flex items-center bg-greyish-300 rounded-xl p-2 relative'>
+		<div className=' flex items-center bg-greyish-300 rounded-xl p-2 relative border border-greyish-200/40'>
+			{attachmentPreview && (
+				<div className='absolute w-[120px] h-[120px] bottom-[4.6rem] left-0 bg-greyish-400/50 p-2 rounded-md '>
+					<img src={attachmentPreview} className='object-cover w-full h-full' />
+					<div
+						className='w-5 h-5 rounded-full bg-Red-100 flex justify-center items-center absolute -right-2 -top-2 cursor-pointer hover:bg-Red-200'
+						onClick={() => {
+							setAttachment(null);
+							setAttachmentPreview(null);
+						}}>
+						<IoClose size={14} color='#fff' />
+					</div>
+				</div>
+			)}
 			<div className='shrink-0'>
-				<input type='file' id='fileUploader' className='hidden' onChange={''} />
+				<input
+					type='file'
+					id='fileUploader'
+					className='hidden'
+					onChange={onFileChange}
+				/>
 				<label htmlFor='fileUploader'>
 					<Icons
 						size='large'
-						icon={<CgAttachment size={20} className='text-greyish-600/80' />}
+						icon={
+							<CgAttachment
+								size={20}
+								className='w-9 h-9 rounded-full p-2 text-Gray-200/50 hover:bg-greyish-600/80 '
+							/>
+						}
 					/>
 				</label>
 			</div>
@@ -28,7 +74,10 @@ const ChatFooter = () => {
 				<Icons
 					size='large'
 					icon={
-						<HiOutlineEmojiHappy size={25} className='text-greyish-600/80' />
+						<HiOutlineEmojiHappy
+							size={25}
+							className='w-9 h-9 rounded-full p-[6px] text-Gray-200/50 hover:bg-greyish-600/80 '
+						/>
 					}
 					onClick={() => setUseEmojiPicker(true)}
 				/>
@@ -36,8 +85,8 @@ const ChatFooter = () => {
 					<ClickAwayListener onClickAway={handleClickAway}>
 						<div className='absolute bottom-12 left-0 shadow-lg'>
 							<EmojiPicker
-								emojiStyle='native'
-								theme='light'
+								emojiStyle='apple'
+								theme='dark'
 								onEmojiClick={onEmojiClick}
 								autoFocusSearch={false}
 								width={350}
@@ -46,6 +95,23 @@ const ChatFooter = () => {
 					</ClickAwayListener>
 				)}
 			</div>
+			{isTyping && (
+				<div className='absolute -top-6 left-4 bg-greyish-600 w-full h-6'>
+					<div className='flex gap-2 w-full h-full opacity-50 text-sm text-greyish-100'>
+						{`User is Typing`}
+						<img src='/typing.svg' className='text' />
+					</div>
+				</div>
+			)}
+			{editMsg && (
+				<div
+					className='absolute -top-12 left-1/2 shadow-Red-100/50 -translate-x-1/2 bg-Red-100 text-greyish-100 flex items-center gap-2 py-2 px-4 pr-2 rounded-full  font-semibold cursor-pointer shadow-lg hover:bg-Red-200'
+					onClick={() => setEditMsg(null)}>
+					<span className='text-sm'>Cancel Edit</span>
+					<IoClose size={16} color='text-greyish-100 ' />
+				</div>
+			)}
+			<Composebar />
 		</div>
 	);
 };
--- components/Composebar.jsx
+++ components/Composebar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineSend } from 'react-icons/ai'
+import { useChatContext } from '@/context/chatContext';
 
 const Composebar = () => {
+  const { inputText, setInputText } = useChatContext();
+  const handleTyping = (e) => {
+    setInputText(e.target.value);
+  };
+  const onKeyUp = () => { };
   return (
-    <div>Composebar</div>
-  )
-}
+    <div className='flex items-center gap-2 grow'>
+      <input
+        type='text'
+        placeholder='Type a Message'
+        value={inputText}
+        className='grow w-full h-full outline-none px-2 py-3 text-greyish-100/80 bg-greyish-300 placeholder:text-Gray-200/50 text-base'
+        onChange={handleTyping}
+        onKeyUp={onKeyUp}
+      />
+      <button className={`h-10 w-10 rounded-full pl-1 shrink-0 flex justify-center items-center outline-none ${inputText.trim().length > 0 ? 'bg-greyish-600/80' : ''}`}>
+        <AiOutlineSend size={22} className='text-Gray-200/50' />
+      </button>
+    </div>
+  );
+};
 
-export default Composebar
+export default Composebar;
